8eace22f316ed4d1c3959c4ab8828f21b7789c94
nirzaf <mfmfazrin1986@gmail.com>
2019-10-08 07:03:25 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1>Hello {{name}}! How are you?</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
